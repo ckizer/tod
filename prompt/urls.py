0e7ca55a576e5821bd63bfbcb1e489edd07b8110
Laura Madsen <laura@paradox.(none)>
2008-01-06 21:31:23 +0000

    changed formatting on the prompt list

--- prompt/urls.py
+++ prompt/urls.py
@@ -1,12 +1,11 @@
 from django.conf.urls.defaults import *
 from tod.prompt.models import Prompt
 urlpatterns = patterns('tod.prompt.views', 
-    (r'^$', 'index'),
     (r'^(?P<prompt_id>\d+)/complete/$', 'complete'),
 )
 
 urlpatterns += patterns('',
-    (r'^list/$', 'django.views.generic.list_detail.object_list',  {'queryset': Prompt.objects.all(),}),
-    (r'^create/$', 'django.views.generic.create_update.create_object', {'model': Prompt, 'post_save_redirect': '/prompt/list/'}),
-    (r'^(?P<object_id>\d+)/delete/$', 'django.views.generic.create_update.delete_object', {'model': Prompt, 'post_delete_redirect': '/prompt/list/'}),
+    (r'^$', 'django.views.generic.list_detail.object_list',  {'queryset': Prompt.objects.all(),}),
+    (r'^create/$', 'django.views.generic.create_update.create_object', {'model': Prompt, 'post_save_redirect': '/prompt/'}),
+    (r'^(?P<object_id>\d+)/delete/$', 'django.views.generic.create_update.delete_object', {'model': Prompt, 'post_delete_redirect': '/prompt/'}),
 )
