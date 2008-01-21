f3274e94aaddf4f938cec233b14551bcaa048bb0
Peter Stradinger <peter@cutopia.(none)>
2008-01-20 17:33:18 -0800

    restored good game urls

--- game/urls.py
+++ game/urls.py
@@ -1,10 +1,19 @@
 from django.conf.urls.defaults import *
 
-from viking.story.models import Story
-urlpatterns = patterns('viking.story.views',
-    (r'^(?P<story_id>\d+)/story/$', 'story'),
+from tod.game.models import Game
+urlpatterns = patterns('tod.game.views',
+    (r'^(?P<game_id>\d+)/play/$', 'play'),
+    (r'^(?P<game_id>\d+)/game_over/$', 'game_over'),
+    (r'^(?P<game_id>\d+)/complete/$', 'complete'),
+    (r'^(?P<game_id>\d+)/wimp_out/$', 'wimp_out'),
+    (r'^(?P<game_id>\d+)/begin_game/$', 'begin_game'),  
+    (r'^(?P<game_id>\d+)/$', 'detail'),
+    (r'^(?P<game_id>\d+)/players_added/$', 'players_added'),
+    (r'^(?P<game_id>\d+)/select_prompts/$', 'select_prompts'),
 )
 
 urlpatterns += patterns ('',
-    (r'^$', 'django.views.generic.list_detail.object_list', {'queryset': Story.objects.all(),'allow_empty': True}),
+    (r'^$', 'django.views.generic.list_detail.object_list', {'queryset': Game.objects.all(),'allow_empty': True}),
+    (r'^create/$', 'django.views.generic.create_update.create_object', {'model': Game}),
+    (r'^(?P<object_id>\d+)/delete/$', 'django.views.generic.create_update.delete_object', {'model': Game, 'post_delete_redirect': '/game/'}),
 )
