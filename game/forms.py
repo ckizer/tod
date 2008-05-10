9d545ce90c0f7bdd0763aec6bf32a43d819553f4
Peter Stradinger <peter@cutopia.(none)>
2008-05-10 08:04:05 -0700

    documented game forms file

--- game/forms.py
+++ game/forms.py
@@ -1,8 +1,16 @@
 from django.newforms import ModelForm
 from django import newforms as forms
+
 from tod.game.models import Game
 
 class GameForm(ModelForm):
+    """Provides a form for creating a game
+
+    TODO - test that the form properly creates a game object with specified name
+    TODO - test that the form properly creates a game object with specified max_difficulty
+    TODO - test that the form properly creates a game object with specified tagged items
+
+    """
     class Meta:
         model = Game
         fields = ('name', 'max_difficulty')
