59adbb82e2b7f8337ff2c2344bb98bc70d45ba98
Peter Stradinger <peter@cutopia.(none)>
2008-05-10 07:31:31 -0700

    documented game urls file

--- game/urls.py
+++ game/urls.py
@@ -2,17 +2,21 @@ from django.conf.urls.defaults import *
 
 from tod.game.models import Game
 urlpatterns = patterns('tod.game.views',
-    (r'^(?P<game_id>\d+)/play/(?P<choice>(truth|dare))/$', 'play'),
-    (r'^(?P<game_id>\d+)/choice/$', 'choice'),
-    (r'^(?P<game_id>\d+)/game_over/$', 'game_over'),
-    (r'^(?P<game_id>\d+)/complete/$', 'complete'),
-    (r'^(?P<game_id>\d+)/wimp_out/$', 'wimp_out'),
-    (r'^(?P<game_id>\d+)/begin_game/$', 'begin_game'),  
+#display objects
+    (r'^$', 'object_list'),
     (r'^(?P<game_id>\d+)/$', 'detail'),
+#create process
+    (r'^create/$', 'create_object'),
     (r'^(?P<game_id>\d+)/players_added/$', 'players_added'),
     (r'^(?P<game_id>\d+)/select_prompts/$', 'select_prompts'),
-    (r'^$', 'object_list'),
-    (r'^create/$', 'create_object'),
+#gameplay flow
+    (r'^(?P<game_id>\d+)/begin_game/$', 'begin_game'),  
+    (r'^(?P<game_id>\d+)/choice/$', 'choice'),
+    (r'^(?P<game_id>\d+)/play/(?P<choice>(truth|dare))/$', 'play'),
+    (r'^(?P<game_id>\d+)/complete/$', 'complete'),
+    (r'^(?P<game_id>\d+)/wimp_out/$', 'wimp_out'),
+    (r'^(?P<game_id>\d+)/game_over/$', 'game_over'),
+#delete process
     (r'^(?P<object_id>\d+)/delete/$', 'limited_delete_object', {'model': Game, 'post_delete_redirect': '/game/'}),
 )
 
