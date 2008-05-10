5f1efe8c42b326bdef8012d9fc5f838c0ddb70d7
Peter Stradinger <peter@cutopia.(none)>
2008-05-10 08:01:05 -0700

    documented game views file

--- game/views.py
+++ game/views.py
@@ -1,26 +1,33 @@
 from django.contrib.auth.decorators import login_required
-
 from django.shortcuts import render_to_response, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.template import RequestContext
-from django.newforms import form_for_model
-from django.views.generic.list_detail import object_list
 from django.views.generic.create_update import delete_object
+
 from tod.game.forms import GameForm
 from tod.game.models import Game
 from tod.prompt.models import Prompt
 
 @login_required
 def object_list(request):
+    """displays a list of game objects
+
+    TODO - test that game objects are displayed
+    """
     template = "game/game_list.html"
     context = {'object_list':request.user.game_set.all()}
     return render_to_response(template, context, context_instance=RequestContext(request))
 
 @login_required
 def create_object(request):
+    """creates a game object if there is a post, otherwise displays the game form
+
+    TODO - test that object is created if there is a post
+    TODO - test that game form displays if there is not a post
+    """
     if request.method == "POST":
-        values = request.POST.copy()
-        form = GameForm(values)
+        #TODO - add user to GameForm
+        form = GameForm(request.POST.copy())
         if form.is_valid():
             game = form.save(commit=False)
             game.user = request.user
@@ -28,16 +35,24 @@ def create_object(request):
             return HttpResponseRedirect(game.get_absolute_url())
     else:
         form = GameForm()
-    context = {'form': form}
     template = "game/game_form.html"
-    return render_to_response(template, context, context_instance=RequestContext(request))
+    return render_to_response(template, locals(), context_instance=RequestContext(request))
 
 @login_required
 def limited_delete_object(*args, **kwargs):
+    """generic delete limited by login
+
+    TODO - test that object is deleted
+    """
     return delete_object(*args, **kwargs)
 
 @login_required
 def detail(request, game_id):
+    """tells game where to redirect according to game status
+
+    TODO - test that games with each status will redirect to the correct url's
+    TODO - use named urls to avoid violating the DRY principle
+    """
     game = get_object_or_404(Game, pk=game_id)
     #define a dictionary of redirects depending on the status of the game
     game_status = {
@@ -47,23 +62,29 @@ def detail(request, game_id):
         'in_progress': '/game/%s/choice/' % game_id,
         'completed': '/game/%s/game_over/' % game_id,
         }
-
     return HttpResponseRedirect(game_status[game.status])
 
 @login_required
 def select_prompts(request, game_id):
+    """chooses which prompts are assigned to the game
+    
+    determine the number of rounds
+    determine filtering rules by tag
+    determine max_difficulty
+    TODO - put filtering functionality into the model
+    """
     game = get_object_or_404(Game, pk=game_id)
 
     template = "game/select_prompts.html"
     player_count = game.players.count()
     error = ""
+    #TODO - availablePrompts instead of Prompt.objects.count()
     if player_count:
         maximum_rounds = Prompt.objects.count() / player_count
     else:
         maximum_rounds = 0
         error = "MINIMUM_PLAYERS_EXCEEDED"
-    context = {"game": game, "maximum_rounds": maximum_rounds, "error": error}
-    if request.method == "POST":
+       if request.method == "POST":
         values = request.POST.copy()
         rounds = int(values['rounds'])
         if 0 < rounds <= maximum_rounds:
@@ -71,13 +92,20 @@ def select_prompts(request, game_id):
             return HttpResponseRedirect(game.get_absolute_url())
         else:
             if rounds<1:
-                context["error"] = "MINIMUM_ROUNDS_EXCEEDED"
+                error = "MINIMUM_ROUNDS_EXCEEDED"
             else:
-                context["error"] = "MAXIMUM_ROUNDS_EXCEEDED"
-    return render_to_response(template, context, context_instance=RequestContext(request))
+                error = "MAXIMUM_ROUNDS_EXCEEDED"
+    return render_to_response(template, locals(), context_instance=RequestContext(request))
 
 @login_required
 def begin_game(request, game_id):
+    """Displays game rules and gives a form to start the game
+
+    a post will change the game status
+    TODO - test that a post will change the game status to in_progress
+    TODO - test that a form is displayed to start the game
+    TODO - test that the rules are displayed
+    """
     game = get_object_or_404(Game, pk=game_id)
     rules = file("game/rules.txt").read()
     template = "game/begin_game.html"
@@ -89,6 +117,13 @@ def begin_game(request, game_id):
 
 @login_required
 def choice(request, game_id):
+    """Displays the truth or dare choice
+
+    sends to game over page if there are no more prompts
+    TODO - test that an in progress game redirects to game over if there are no more prompts
+    TODO - test that an in progress game's status is changed to game_over if there are no more prompts
+    TODO - test that a form is displayed presenting a choice
+    """
     game = get_object_or_404(Game, pk=game_id)
     template = "game/choice.html"
     if not game.current_prompt():
@@ -100,6 +135,11 @@ def choice(request, game_id):
     
 @login_required
 def play(request, game_id, choice):
+    """Displays the prompt with the given choice and a form for finishing the prompt
+
+    TODO - test that the correct prompt is displayed for the choice given
+    TODO - test that a form is displayed to wimp out or complete
+    """
     game = get_object_or_404(Game, pk=game_id)
     template = "game/play.html"
     current_prompt = game.current_prompt().prompt
@@ -109,6 +149,10 @@ def play(request, game_id, choice):
 
 @login_required
 def wimp_out(request, game_id):
+    """Finishes the prompt without adding to the score
+
+    TODO - test that the prompt is finished and the score of the player is not changed
+    """
     game = get_object_or_404(Game, pk=game_id)
     current_prompt = game.current_prompt()
     current_prompt.complete()
@@ -116,6 +160,10 @@ def wimp_out(request, game_id):
 
 @login_required
 def complete(request, game_id):
+    """Finishes the prompt and updates the player's score
+
+    TODO - test that the prompt is finished and the score of the player is changed
+    """
     game = get_object_or_404(Game, pk=game_id)
     current_prompt = game.current_prompt()
     current_player = game.current_player()
@@ -125,6 +173,11 @@ def complete(request, game_id):
 
 @login_required
 def game_over(request, game_id):
+    """Displays the game over page and the winner
+
+    TODO - test that the game over page is displayed
+    TODO - test that the winner is displayed
+    """
     game = get_object_or_404(Game, pk=game_id)
     template = "game/over.html"
     context = {"players": game.players.all(), "game": game}
@@ -132,6 +185,10 @@ def game_over(request, game_id):
 
 @login_required
 def players_added(request, game_id):
+    """Changes game status to players_added
+
+    TODO - test that game status is changed to players_added
+    """
     game = get_object_or_404(Game, pk=game_id)
     game.players_added()
     return HttpResponseRedirect(game.get_absolute_url())
