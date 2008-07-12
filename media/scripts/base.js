a9286584009b525973263dea80a7076b6d6f0ad0
Peter Stradinger <peter@cutopia.(none)>
2008-07-12 01:42:02 -0700

    jquery scripting to show and hide the comment form.

---
+++ media/scripts/base.js
@@ -0,0 +1,11 @@
+function toggle_comment_form(event) {
+    console.log("clicked toggle_comment_form");
+    $("#comment_form").slideToggle();
+    event.preventDefault();
+}
+
+function assign_behaviors() {
+    $("#comment_area a").unbind("click").bind("click", toggle_comment_form);
+}
+
+$(document).ready(assign_behaviors);
