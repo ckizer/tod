475690389607f6797edeb6ceb19bd220073b48ef
Laura Madsen <lauramadsen@Juanita.local>
2009-05-29 19:23:55 -0700

    removed cruft button images, added a thank you alert after a comment is sent

--- media/scripts/base.js
+++ media/scripts/base.js
@@ -4,9 +4,16 @@ function toggle_comment_form(event) {
     event.preventDefault();
 }
 
+    function confirm_comment(event){
+//	console.log("clicked submit button");
+	alert("Thank you for your input");
+	toggle_comment_form;
+    }
+
 function assign_behaviors() {
     $("#comment_area a").unbind("click").bind("click", toggle_comment_form);
     $("#cancel_comment").unbind("click").bind("click", toggle_comment_form);
+    $("#submit_comment").unbind("click").bind("click", confirm_comment);
 }
 
 $(document).ready(assign_behaviors);
