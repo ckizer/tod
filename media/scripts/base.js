6f5c09248173b39803af8c91994af5c52c5f8d2a
Peter <peter@rightscale.com>
2009-06-05 22:07:01 -0700

    added in the javascript to make the focus of the form.

--- media/scripts/base.js
+++ media/scripts/base.js
@@ -1,11 +1,11 @@
 function toggle_comment_form(event) {
-//    console.log("clicked toggle_comment_form");
+    /*    console.log("clicked toggle_comment_form"); */
     $("#comment_form").slideToggle();
     event.preventDefault();
 }
 
     function confirm_comment(event){
-//	console.log("clicked submit button");
+	/*	console.log("clicked submit button"); */
 	alert("Thank you for your input");
 	toggle_comment_form;
     }
@@ -27,16 +27,50 @@ function toggle_comment_form(event) {
 	event.target.src = event.target.src.replace("_down.png","_over.png");
     }
 
-function assign_behaviors() {
-    $("#comment_area a").unbind("click").bind("click", toggle_comment_form);
-    $("#cancel_comment").unbind("click").bind("click", toggle_comment_form);
-    $("#submit_comment").unbind("click").bind("click", confirm_comment);
-    $(".stated_button").unbind("mouseover").bind("mouseover", button_mouse_over);
-    $(".stated_button").unbind("mouseout").bind("mouseout", button_mouse_out);
-    $(".stated_button").unbind("mousedown").bind("mousedown", button_mouse_down);
-    $(".stated_button").unbind("mouseup").bind("mouseup", button_mouse_up);
-    $(".stated_button").unbind("drag").bind("drag", button_mouse_out);
+    function assign_behaviors() {
+	$("#comment_area a").unbind("click").bind("click", toggle_comment_form);
+	$("#cancel_comment").unbind("click").bind("click", toggle_comment_form);
+	$("#submit_comment").unbind("click").bind("click", confirm_comment);
+	$(".stated_button").unbind("mouseover").bind("mouseover", button_mouse_over);
+	$(".stated_button").unbind("mouseout").bind("mouseout", button_mouse_out);
+	$(".stated_button").unbind("mousedown").bind("mousedown", button_mouse_down);
+	$(".stated_button").unbind("mouseup").bind("mouseup", button_mouse_up);
+	$(".stated_button").unbind("drag").bind("drag", button_mouse_out);
 
-}
+    }
+    
+    function set_focus() {
+	var start_heres = $(".start_here");
+	var element_to_focus = false;
+	if(start_heres) {
+	    var first_start_here = start_heres[0];
+	    if(first_start_here) {
+		if(first_start_here.is("input")) {
+		    // The first marked element is the start here
+		    element_to_focus = first_start_here;
+		} else {
+		    // Find the first input in the start here block element
+		    var contained_element = first_start_here.find("input");
+		    if(contained_element) {
+			element_to_focus = contained_element
+		    }
+		}
+	    }
+	}
+	var forms = $("form");
+	if(forms) {
+	    var inputs = forms.find("input");
+	    if( inputs ) {
+		var first_input = inputs[0];
+		if( $(first_input).is("input")) {
+		    element_to_focus = first_input;
+		}
+	    }
+	}
+	if(element_to_focus) {
+	    element_to_focus.focus();
+	}
+    }
 
-$(document).ready(assign_behaviors);
+    $(document).ready(assign_behaviors);
+    $(document).ready(set_focus);
