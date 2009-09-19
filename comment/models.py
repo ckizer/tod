bbf7752456054d7080d84a6986f6347d15e3cac2
Create a User <tod@beyondparadox.(none)>
2009-09-19 16:42:37 +0000

    Making the prompt for comments better.

--- comment/models.py
+++ comment/models.py
@@ -6,7 +6,7 @@ class Comment(models.Model):
     """Provides functionality for the comment object
     """
     page = models.CharField(max_length=50, editable = False)
-    description = models.TextField(help_text="Please describe the problem you're seeing.")
+    description = models.TextField(help_text="Please enter your comment for the site administrator.")
     email = models.EmailField()
     created = models.DateTimeField(auto_now_add=True, null=False, blank=True, editable=False)
     
